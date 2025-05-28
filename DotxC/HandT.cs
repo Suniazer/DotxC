0d52cbe46051709a31bef2650fd2710c68d4d013
sunia <sunia@SUNIAZER>
2025-05-28 17:56:59 +0800

    添加项目文件。

---
+++ DotxC/HandT.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Office.Interop.Word;
+using System.Collections.Generic; 
+
+namespace DotxC
+{
+    public static class HandT
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tpath">模板位置</param>
+        /// <param name="spath">保存位置</param>
+        /// <param name="pairs">键值对</param>
+        public static void Replace(string tpath, string spath, Dictionary<string, string> pairs)
+        {
+            // 创建一个新的Word文档对象
+            Application wordApp = new Application();
+            Document wordDoc = wordApp.Documents.Add();
+            // 打开Word模板文件
+            object oTemplate = tpath;
+            wordDoc = wordApp.Documents.Add(ref oTemplate);
+            // 将模板文件中的内容复制到新的Word文档对象中
+            wordDoc.Content.InsertParagraphAfter();
+            wordDoc.Content.InsertFile(oTemplate.ToString());
+            // 将新的内容替换为您需要的内容
+            foreach (var pair in pairs)
+            {
+                FindAndReplace(wordDoc, pair.Key, pair.Value);
+            }
+            // 保存新的Word文档
+            object oFilename = spath;
+            wordDoc.SaveAs2(ref oFilename);
+            wordApp.Quit();
+        }
+
+        static void FindAndReplace(Document doc, string findText, string replaceWithText)
+        {
+            // 将新的内容替换为您需要的内容
+            foreach (Range range in doc.StoryRanges)
+            {
+                range.Find.Execute(FindText: findText, ReplaceWith: replaceWithText,
+                    MatchWholeWord: true, Replace: WdReplace.wdReplaceAll);
+            }
+        }
+    }
+}
---
+++ DotxC/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DotxC
+{
+    internal class Program
+    {
+        private static string SelectFile(string filter, string title, bool multi = false)
+        {
+        re:
+
+            OpenFileDialog OpenFD = new OpenFileDialog
+            {
+                Multiselect = multi,
+                Filter = filter,
+                Title = title
+            };
+            if (!(bool)OpenFD.ShowDialog()) { goto re; }
+            return OpenFD.FileName;
+        }
+        [STAThread]
+        static void Main(string[] args)
+        {
+        start:
+            Console.WriteLine("模板位置：");
+            string tpath = SelectFile("文档模板|*.dotx", "选取文档模板");
+            //if (Console.ReadKey().Key != ConsoleKey.Enter) { goto start; }
+            Console.Write(tpath + "\r\n");
+            Console.WriteLine();
+            Dictionary<string ,string > pairs= new Dictionary<string ,string>();
+        rpair:
+            Console.WriteLine("键值对位置：");
+            string ppath = SelectFile("键值对|*.txt", "选取文本");
+            //if (Console.ReadKey().Key != ConsoleKey.Enter) { goto rpair; }
+            Console.Write(ppath + "\r\n");
+            Console.WriteLine();
+            foreach (var line in File.ReadAllLines(ppath))
+            {
+                try
+                {
+                    string[] strings= line.Split(new char[] {'|' });
+                    if (strings.Length != 2) { continue; }
+                    pairs.Add(strings[0].Trim(), strings[1].Trim());
+                }
+                catch(Exception ex) { Console.WriteLine(ex.Message); goto rpair; }  
+            }
+            foreach (KeyValuePair<string, string> pair in pairs)
+            {
+                Console.WriteLine($"[{pair.Key}]:[{pair.Value}]");
+            }
+        rsave:
+            Console.WriteLine("保存位置：");
+            SaveFileDialog SaveFD = new SaveFileDialog();
+            SaveFD.Filter = "文档|*.doc";
+            if (!(bool)SaveFD.ShowDialog()) { goto rsave; }
+            string spath = SaveFD.FileName;
+            Console.WriteLine($"{spath}");
+            try
+            {
+                HandT.Replace(tpath, spath, pairs);
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); goto start; }
+            Console.WriteLine();
+            Console.WriteLine("[结束]");
+            Console.ReadLine();
+        }
+    }
+}
